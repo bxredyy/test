72900be47248edb53a628cecc4a5e9c18afa2769
bxredyy <122664806+bxredyy@users.noreply.github.com>
2026-03-19 14:49:19 +0200

    intitial push

---
+++ TODOLi/TODOLi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TODOLi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
